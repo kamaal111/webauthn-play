9dfc948352eb8779c28890f735b7a9d2248f3cca
Kamaal <kamaal.f1@gmail.com>
2023-02-04 20:52:59 +0100

    Initializing web app

---
+++ web/src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <meta name="description" content="Web authn play" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <p>Hallo</p>
+      </main>
+    </>
+  )
+}
