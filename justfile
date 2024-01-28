bde9b963bdfa48f342fd50189259a768ab1b2bac
Kamaal Farah <kamaal.f1@gmail.com>
2024-01-28 15:37:09 +0000

    Initializing website

--- justfile
+++ justfile
@@ -4,8 +4,12 @@ default:
 run-dev-server:
 	just server/run-dev
 
+run-dev-website:
+	just website/run-dev
+
 migrate-server:
 	just server/migrate
 
 bootstrap:
 	just server/bootstrap
+	just website/bootstrap
---
+++ website/src/pages/index.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>WebAuthn play</title>
+        <meta
+          name="description"
+          content="Playing and learning about web authn"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>Kamaal</h1>
+      </main>
+    </>
+  );
+}
